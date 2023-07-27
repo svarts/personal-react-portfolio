ee474d1a482cbbe4ecfcb232189b577b2b22ff13
svartse <killbillbeatrixx@gmail.com>
2023-07-27 16:28:54 +0300

    md :s

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {useEffect} from 'react'
 import Navbar from './components/Navbar';
 import Hiro from './components/Hiro';
 import Skills from './components/Skills'
-import Honors from './components/Honors';
 import Certs from './components/Certs';
 import Footer from './components/Footer';
 import AOS from 'aos';
@@ -20,7 +19,6 @@ function App() {
       <Navbar />
       <Hiro />
       <Skills />
-      <Honors />
       <Certs />
       <Footer />
     </div>
--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -8,7 +8,7 @@ import hr from '../assets/curve-hr.svg';
 export default function Hiro () {
 
     const [loaded, setLoaded] = useState(true);
-
+    
     return (
         <>
         {loaded ?
@@ -20,14 +20,14 @@ export default function Hiro () {
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
             <div className='md:w-3/6 md:p-4'>
-                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profileText} alt="profile" onLoad={() => setLoaded(false)} />
+                <img className='xs:mt-20' data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profileText} alt="profile" onLoad={() => setLoaded(false)} />
             </div> 
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">
                     <h1 className="text-3xl font-extrabold bg-gradient-to-r from-amber-200 via-pink-200 to-indigo-400 inline-block text-transparent bg-clip-text">Hi, I'm</h1>
                     <h1 className="text-4xl font-bold bg-gradient-to-r from-amber-200 via-pink-200 to-indigo-400 inline-block text-transparent bg-clip-text">Esma Karaca</h1>
                     <p class="text-2xl font-bold bg-gradient-to-r from-amber-200 via-pink-200 to-indigo-400 inline-block text-transparent bg-clip-text mb-5">Software Developer</p>
-                    <p className="text-xl font-light bg-gradient-to-r from-amber-200 via-pink-200 to-indigo-400 inline-block text-transparent bg-clip-text mb-5">I'm proficient in a wide range of programming languages and frameworks, I have a strong passion for creating robust, scalable, and user-friendly applications. My solid understanding of software development lifecycle and best practices allows me to consistently deliver high-quality code and meet project deadlines.</p>
+                    <p className="text-xl font-light bg-gradient-to-r from-amber-200 via-pink-200 to-indigo-400 inline-block text-transparent bg-clip-text mb-5">I develop attractive user interfaces and web applications proficient in a wide range of programming languages and frameworks. I have a strong passion for creating robust, scalable, and user-friendly applications. My solid understanding of software development lifecycle and best practices allows me to consistently deliver high-quality code and meet project deadlines.</p>
                 </div>
                 <a href='' className='mt-2 block '>Reach me <FontAwesomeIcon className='ml-2' icon={faCircleArrowRight}/> </a>
                 
